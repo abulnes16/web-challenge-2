dbde8f33c7106707bf7c0cd6fbbe8c89f959c0e9
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-08-28 20:58:50 -0600

    Añadido interacción con js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/* HTML Elements */
+const socialMediaLinks = document.getElementById("social-media");
+const avatar = document.getElementById("avatar");
+const cardFooter = document.getElementById("card-footer");
+const shareButton = document.getElementById("share-button");
+let showMedia = false;
+
+shareButton.onclick = () => {
+  if (!showMedia) {
+    socialMediaLinks.style.display = "initial";
+    avatar.style.display = "none";
+  } else {
+    avatar.style.display = "inherit";
+    socialMediaLinks.style.display = "none";
+  }
+  cardFooter.classList.toggle("active-share");
+  shareButton.classList.toggle("active-share-button");
+  showMedia = !showMedia;
+};
